869cf1a44cd2e0c3ff2f9efcd4d800859135ddf2
Victor Fernandes <roviczzf@gmai.com>
2022-08-24 19:57:12 -0300

    querrySelector's and others

--- DOM Learning/DOM-learning.js
+++ DOM Learning/DOM-learning.js
@@ -21,8 +21,27 @@ console.log('O elemento está ativo!')
 
 // Retorne as linguagens do navegador
 const linguagensAtivas = navigator.language;
-console.log(`A(s) linguagem(ns) ativa(s) no navegador é(são) ${linguagensAtivas}.`)
+console.log(`A(s) linguagem(ns) ativa(s) no navegador é(são) ${linguagensAtivas}.`);
 
 // Retorne a largura da janela
 const largura = window.innerWidth;
-console.log(`A largura da janela é de ${largura}px.`)
+console.log(`A largura da janela é de ${largura}px.`);
+
+// Atividade 02
+
+// Retorne todas as imagens do site.
+const imgAll = document.querySelectorAll('img');
+console.log(`A quantidade de imagens no site é ${imgAll.length}.`);
+
+// Retorne no console apenas imagens que possuem o nome de imagem.
+const onlyImagens = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(`A quantidade de imagens com o nome de "imagem" é ${onlyImagens}.`)
+
+// Selecione os links internos (onde o href começa com #).
+const linkInterno = document.querySelectorAll('img[href^="#"]');
+console.log(`O link interno é o ${linkInterno}.`)
+
+// Selecione o último p do site.
+const ultimoP = document.querySelectorAll('p')
+console.log(ultimoP[--ultimoP.length]);
+
