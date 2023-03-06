abad12105f9e416195060300892237f96fe8291b
Victor Fernandes <roviczzf@gmail.com>
2023-03-06 18:49:02 -0300

    Get e Set

---
+++ Classes/Get e Set/js/script.js
@@ -0,0 +1,39 @@
+const button = {
+  get tamanho() {
+    return this._numero || 200;
+  }, 
+  set tamanho(numero) {
+    this._numero = numero + 'px';
+  }
+}
+
+const frutas = {
+  lista: [], 
+  set nova(fruta) {
+    this.lista.push(fruta);
+  }
+}
+
+frutas.nova = 'Pera'
+frutas.nova = 'Maça'
+
+// Teste 01
+
+class Button {
+  constructor(text, color) {
+    this.text = text;
+    this.color = color;
+  }
+  get element() {
+    const type = this._elementType || 'button';
+    const buttonElement = document.createElement(type);
+    buttonElement.innerText = this.text;
+    buttonElement.style.color = this.color;
+    return buttonElement;
+  }
+  set element(type) {
+    this._elementType = type;
+  }
+}
+
+const blueButton = new Button('Comprar', 'Blue');
