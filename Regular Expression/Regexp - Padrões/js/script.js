13d53e9d7729347718f2a41cc2fb88a83470b893
Victor Fernandes <roviczzf@gmail.com>
2023-03-15 14:07:23 -0300

    Regexp - Padrões 2/2

--- Regular Expression/Regexp - Padrões/js/script.js
+++ Regular Expression/Regexp - Padrões/js/script.js
@@ -92,3 +92,38 @@ for(email of emails) {
  // console.log(email, email.match(regexpEMAIL));
 }
 
+// TAG
+
+const regexpTAG = /<\/?[\w\s="']+\/?>/gi;
+
+const tags = [
+  '<div>Isso é uma div</div>',
+  '<div class="ativa">Essa está ativa</div>',
+  '<img src="imagem" />',
+  '<img src="imagem">',
+  '<ul class="ativa">',
+  '<li>Essa está ativa</li>',
+  '</ul>'
+];
+
+for(tag of tags) {
+ // console.log(tag, tag.match(regexpTAG));
+}
+
+// TAG apenas para Nome
+
+const regexpTAG2 = /(?<=<\/?)[\w]+/gi;
+
+const tags2 = [
+  '<div>Isso é uma div</div>',
+  '<div class="ativa">Essa está ativa</div>',
+  '<img src="imagem" />',
+  '<img src="imagem">',
+  '<ul class="ativa">',
+  '<li>Essa está ativa</li>',
+  '</ul>'
+];
+
+for(tag of tags2) {
+ // console.log(tag, tag.match(regexpTAG));
+}
